86a180b498cd142a6ebda23851f8d5f72248c217
owenliversidge <owen.liversidge@gmail.com>
2019-02-12 15:57:12 -0500

    app now working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-<<<<<<< HEAD
 import styled from "styled-components";
-=======
->>>>>>> ef0eb109953fcea77baf8fdfa956dee64e8999f3
 
 import Marketplace from "./pages/marketplace-page/Marketplace";
 import Cart from "./pages/cart-page/Cart";
@@ -12,7 +9,6 @@ import Billing from "./pages/billing-page/Billing";
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <Appstyles>
         <Router>
           <div>
@@ -27,20 +23,6 @@ class App extends Component {
           </Switch>
         </Router>
       </Appstyles>
-=======
-      <Router>
-        <div>
-          <Link to={"/"}>Marketplace</Link>
-          <Link to={"/cart"}>Cart</Link>
-          <Link to={"/billing"}>Billing</Link>
-        </div>
-        <Switch>
-          <Route exact path={"/"} component={Marketplace} />
-          <Route exact path={"/cart"} component={Cart} />
-          <Route exact path={"/billing"} component={Billing} />
-        </Switch>
-      </Router>
->>>>>>> ef0eb109953fcea77baf8fdfa956dee64e8999f3
     );
   }
 }

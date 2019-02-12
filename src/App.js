1f76e727cdc1483a4e25f038ffa7e0541a82a1ab
owenliversidge <owen.liversidge@gmail.com>
2019-02-12 15:52:43 -0500

    trying to resolve git issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import styled from "styled-components";
+
+import Marketplace from "./pages/marketplace-page/Marketplace";
+import Cart from "./pages/cart-page/Cart";
+import Billing from "./pages/billing-page/Billing";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Appstyles>
+        <Router>
+          <div>
+            <Link to={"/"}>Marketplace</Link>
+            <Link to={"/cart"}>Cart</Link>
+            <Link to={"/billing"}>Billing</Link>
+          </div>
+          <Switch>
+            <Route exact path={"/"} component={Marketplace} />
+            <Route exact path={"/cart"} component={Cart} />
+            <Route exact path={"/billing"} component={Billing} />
+          </Switch>
+        </Router>
+      </Appstyles>
+    );
   }
 }
 
 export default App;
+
+const Appstyles = styled.div`
+  color: black;
+`;

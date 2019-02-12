c2c9ea87372daefe2b668cd9e0ef67172a21c13b
owenliversidge <owen.liversidge@gmail.com>
2019-02-12 15:56:12 -0500

    resolved git issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+<<<<<<< HEAD
 import styled from "styled-components";
+=======
+>>>>>>> ef0eb109953fcea77baf8fdfa956dee64e8999f3
 
 import Marketplace from "./pages/marketplace-page/Marketplace";
 import Cart from "./pages/cart-page/Cart";
@@ -9,6 +12,7 @@ import Billing from "./pages/billing-page/Billing";
 class App extends Component {
   render() {
     return (
+<<<<<<< HEAD
       <Appstyles>
         <Router>
           <div>
@@ -23,6 +27,20 @@ class App extends Component {
           </Switch>
         </Router>
       </Appstyles>
+=======
+      <Router>
+        <div>
+          <Link to={"/"}>Marketplace</Link>
+          <Link to={"/cart"}>Cart</Link>
+          <Link to={"/billing"}>Billing</Link>
+        </div>
+        <Switch>
+          <Route exact path={"/"} component={Marketplace} />
+          <Route exact path={"/cart"} component={Cart} />
+          <Route exact path={"/billing"} component={Billing} />
+        </Switch>
+      </Router>
+>>>>>>> ef0eb109953fcea77baf8fdfa956dee64e8999f3
     );
   }
 }
